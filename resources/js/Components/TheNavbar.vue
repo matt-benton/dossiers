1cdd312be04986deeb710e44693b19cc959cc5a1
matt-benton <mattbenton@hey.com>
2022-04-19 16:43:04 -0500

    Add dropdown to navbar

--- resources/js/Components/TheNavbar.vue
+++ resources/js/Components/TheNavbar.vue
@@ -11,7 +11,28 @@
       </div>
       <div class="navbar-menu-right">
         <ul>
-          <li class="dropdown">johndoe@test.com <ChevronDown /></li>
+          <li>
+            <div class="dropdown">
+              <div
+                class="dropdown-toggle"
+                @click="accountDropDownVisible = true"
+              >
+                johndoe@test.com <ChevronDown />
+              </div>
+              <ul class="dropdown-menu" v-show="accountDropDownVisible">
+                <li><a href="#">Dropdown Link</a></li>
+                <li><a href="#">Dropdown Link</a></li>
+                <li><a href="#">Dropdown Link</a></li>
+              </ul>
+            </div>
+            <Teleport to="body">
+              <div
+                class="dropdown-cover"
+                v-show="accountDropDownVisible"
+                @click="hideAccountDropDown"
+              ></div>
+            </Teleport>
+          </li>
         </ul>
       </div>
     </div>
@@ -21,6 +42,18 @@
 import ChevronDown from './Icons/ChevronDown.vue'
 
 export default {
+  data() {
+    return {
+      accountDropDownVisible: false,
+    }
+  },
+  methods: {
+    hideAccountDropDown(event) {
+      if (!event.target.classList.contains('dropdown-menu')) {
+        this.accountDropDownVisible = false
+      }
+    },
+  },
   components: {
     ChevronDown,
   },
@@ -65,11 +98,42 @@ li {
 }
 
 .dropdown {
-  display: flex;
-  align-items: center;
+  position: relative;
 }
 
-.dropdown svg {
+.dropdown-toggle {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.dropdown-toggle svg {
   margin-left: var(--size-3);
+}
+
+.dropdown-menu {
+  position: absolute;
+  top: var(--size-8);
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-2);
+  background-color: var(--cardBg);
+  border-radius: 5px;
+  padding: var(--size-4) var(--size-5);
+  z-index: 9999;
+}
+
+.dropdown-menu li a {
+  white-space: nowrap;
+}
+
+.dropdown-cover {
+  background: transparent;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
