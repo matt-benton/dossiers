f7cf9ba5dd54327eafa3e8bf06f2aa7a39b3d5eb
matt-benton <mattbenton@hey.com>
2022-07-01 10:36:53 -0500

    Remove welcome component

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,8 @@ use App\Http\Controllers\OccurrenceController;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::middleware(['auth', 'verified'])->group(function () {
-  Route::get('/dashboard', function () {
+  Route::get('/', function () {
       return Inertia::render('Dashboard');
   })->name('dashboard');
 
