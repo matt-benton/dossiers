2e4e07c19d4c09f21e2d7ed34bf0340f8450035d
matt-benton <mattbenton@hey.com>
2022-07-14 14:52:36 -0500

    Fix ts errors in theme button

--- resources/js/Components/ThemeButton.vue
+++ resources/js/Components/ThemeButton.vue
@@ -6,76 +6,68 @@
   </button>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref, onBeforeMount, watch } from 'vue'
 import Sun from './Icons/Sun.vue'
 import Moon from './Icons/Moon.vue'
-export default {
-  data() {
-    return {
-      theme: '',
-    }
-  },
-  created() {
-    this.theme = localStorage.getItem('theme') ?? 'light'
-  },
-  watch: {
-    theme(newVal) {
-      switch (newVal) {
-        case 'light':
-          localStorage.setItem('theme', this.setLightTheme())
-          break
-        case 'dark':
-          localStorage.setItem('theme', this.setDarkTheme())
-          break
-        case 'os':
-          localStorage.setItem('theme', this.setOsTheme())
-      }
-    },
-  },
-  methods: {
-    changeTheme() {
-      switch (this.theme) {
-        case 'light':
-          this.theme = 'dark'
-          break
-        case 'dark':
-          this.theme = 'os'
-          break
-        case 'os':
-          this.theme = 'light'
-      }
-    },
-    setDarkTheme() {
-      let body = document.querySelector('body')
-      body.classList.replace('light', 'dark') ??
-        body.classList.replace('os', 'dark')
 
-      return 'dark'
-    },
-    setLightTheme() {
-      let body = document.querySelector('body')
-      body.classList.replace('dark', 'light') ??
-        body.classList.replace('os', 'light')
+let theme = ref('')
 
-      return 'light'
-    },
-    setOsTheme() {
-      let userPrefDark = window.matchMedia(
-        '(prefers-color-scheme: dark)'
-      ).matches
+onBeforeMount(() => {
+  theme.value = localStorage.getItem('theme') ?? 'light'
+})
 
-      if (userPrefDark) {
-        this.setDarkTheme()
-      } else {
-        this.setLightTheme()
-      }
+watch(theme, (newVal) => {
+  switch (newVal) {
+    case 'light':
+      localStorage.setItem('theme', setLightTheme())
+      break
+    case 'dark':
+      localStorage.setItem('theme', setDarkTheme())
+      break
+    case 'os':
+      localStorage.setItem('theme', setOsTheme())
+  }
+})
 
-      return 'os'
-    },
-  },
-  components: {
-    Sun,
-    Moon,
-  },
+const changeTheme = function () {
+  switch (theme.value) {
+    case 'light':
+      theme.value = 'dark'
+      break
+    case 'dark':
+      theme.value = 'os'
+      break
+    case 'os':
+      theme.value = 'light'
+  }
+}
+
+function setDarkTheme() {
+  let body = document.querySelector('body') as Element
+  body.classList.replace('light', 'dark') ??
+    body.classList.replace('os', 'dark')
+
+  return 'dark'
+}
+
+function setLightTheme() {
+  let body = document.querySelector('body') as Element
+  body.classList.replace('dark', 'light') ??
+    body.classList.replace('os', 'light')
+
+  return 'light'
+}
+
+function setOsTheme() {
+  let userPrefDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  if (userPrefDark) {
+    setDarkTheme()
+  } else {
+    setLightTheme()
+  }
+
+  return 'os'
 }
 </script>
