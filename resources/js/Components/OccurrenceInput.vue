63188c4dcef092aaf3b349eb8ec6305f7ca02320
matt-benton <mattbenton@hey.com>
2022-07-14 14:30:24 -0500

    Fix occurrence input errors

--- resources/js/Components/OccurrenceInput.vue
+++ resources/js/Components/OccurrenceInput.vue
@@ -22,29 +22,36 @@
       <li
         v-for="(person, index) in search.results"
         :key="index"
-        @click="
-          selectPerson($event, search.results[search.highlightedResultIndex])
-        "
+        @click="onSearchResultClick"
         :class="{ highlighted: search.highlightedResultIndex === index }"
         @mouseover="search.highlightedResultIndex = index"
       >
-        <Person />
+        <PersonIcon />
         {{ person.name }}
       </li>
     </ul>
   </form>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import axios from 'axios'
 import { useForm } from '@inertiajs/inertia-vue3'
 import { reactive } from 'vue'
-import Person from './Icons/Person.vue'
+import PersonIcon from './Icons/Person.vue'
+import Person from '../Types/Person'
 
 const form = useForm({
   description: '',
 })
 
-let search = reactive({
+interface Search {
+  text: string
+  results: Array<Person>
+  keylogging: boolean
+  highlightedResultIndex: number | null
+}
+
+let search: Search = reactive({
   text: '',
   results: [],
   keylogging: false,
@@ -58,10 +65,16 @@ const submitForm = function () {
 
 const resetForm = function () {
   form.description = ''
-  document.querySelector('#occurrence_text').value = ''
+
+  const occurrenceText: HTMLTextAreaElement | null =
+    document.querySelector('#occurrence_text')
+
+  if (occurrenceText) {
+    occurrenceText.value = ''
+  }
 }
 
-const onInput = function (event) {
+const onInput = function (event: Event) {
   // ! This only really works in ideal scenarios. In
   // ! many cases this will fail and there will be
   // ! no search. But I have decided that usually
@@ -69,7 +82,8 @@ const onInput = function (event) {
   // ! leave it in. When it works it is better than
   // ! anything else.
   const oldText = form.description
-  const newText = event.target.value
+  const target = event.target as HTMLTextAreaElement
+  const newText = target.value
 
   // find the difference between the old text and new text
   const numAddedChars = textWasAdded(oldText, newText)
@@ -77,7 +91,7 @@ const onInput = function (event) {
   if (numAddedChars) {
     const added = getAddedText(oldText, newText, numAddedChars)
 
-    if (search.keylogging) {
+    if (search.keylogging && added.pos) {
       search.text = insertNewText(search.text, added.pos, added.text)
     }
 
@@ -120,11 +134,18 @@ const onInput = function (event) {
   }
 }
 
-const onKeydown = function (event) {
+const onKeydown = function (event: KeyboardEvent) {
   if (search.keylogging) {
     if (event.key === 'Tab') {
       event.preventDefault()
-      selectPerson(event, search.results[search.highlightedResultIndex])
+
+      if (!search.highlightedResultIndex) {
+        if (search.results.length > 0) {
+          selectPerson(search.results[0])
+        }
+      } else {
+        selectPerson(search.results[search.highlightedResultIndex])
+      }
     }
   }
 
@@ -143,7 +164,10 @@ const onKeydown = function (event) {
 }
 
 const nextResult = function () {
-  if (search.highlightedResultIndex + 1 === search.results.length) {
+  if (
+    search.highlightedResultIndex === null ||
+    search.highlightedResultIndex + 1 === search.results.length
+  ) {
     search.highlightedResultIndex = 0
   } else {
     search.highlightedResultIndex += 1
@@ -151,28 +175,37 @@ const nextResult = function () {
 }
 
 const prevResult = function () {
-  if (search.highlightedResultIndex === 0) {
+  if (!search.highlightedResultIndex) {
     search.highlightedResultIndex = search.results.length - 1
   } else {
     search.highlightedResultIndex -= 1
   }
 }
 
-const textWasAdded = function (oldText, newText) {
+const textWasAdded = function (oldText: string, newText: string) {
   const numAddedChars = newText.length - oldText.length
 
   return numAddedChars > 0 ? numAddedChars : 0
 }
 
-const getAddedText = function (oldText, newText, numAddedChars) {
-  for (let i in newText) {
-    if (oldText[i] !== newText[i]) {
-      return { text: newText.slice(i, i + numAddedChars), pos: parseInt(i) }
-    }
-  }
+const getAddedText = function (
+  oldText: string,
+  newText: string,
+  numAddedChars: number
+): { text: string; pos: number | null } {
+  const startIndex = newText
+    .split('')
+    .findIndex((letter, i) => oldText[i] !== letter)
+
+  return startIndex >= 0
+    ? {
+        text: newText.slice(startIndex, startIndex + numAddedChars),
+        pos: startIndex,
+      }
+    : { text: '', pos: null }
 }
 
-const insertNewText = function (str, pos, newText) {
+const insertNewText = function (str: string, pos: number, newText: string) {
   return (
     str.substring(0, pos - 1) +
     newText +
@@ -180,19 +213,34 @@ const insertNewText = function (str, pos, newText) {
   )
 }
 
-const getDeletedText = function (oldText, newText, numDeletedChars) {
-  for (let i in oldText) {
-    if (oldText[i] !== newText[i]) {
-      return oldText.slice(i, parseInt(i) + numDeletedChars)
-    }
+const getDeletedText = function (
+  oldText: string,
+  newText: string,
+  numDeletedChars: number
+): string {
+  const startIndex = oldText
+    .split('')
+    .findIndex((letter, i) => letter !== newText[i])
+
+  return startIndex >= 0
+    ? oldText.slice(startIndex, startIndex + numDeletedChars)
+    : ''
+}
+
+function onSearchResultClick() {
+  if (search.highlightedResultIndex) {
+    selectPerson(search.results[search.highlightedResultIndex])
   }
 }
 
-const selectPerson = function (event, person) {
+const selectPerson = function (person: Person) {
   form.description = form.description.replace(search.text, person.name)
   resetSearch()
-  document.querySelector('#occurrence_text').value = form.description
-  document.querySelector('#occurrence_text').focus()
+  const occurrenceTextArea = document.querySelector(
+    '#occurrence_text'
+  ) as HTMLTextAreaElement
+  occurrenceTextArea.value = form.description
+  occurrenceTextArea.focus()
 }
 
 const resetSearch = function () {
@@ -201,19 +249,18 @@ const resetSearch = function () {
   search.keylogging = false
 }
 
-const getStartOfTextDifference = function (oldText, newText) {
-  let newTextArr = newText.split('')
-
+const getStartOfTextDifference = function (
+  oldText: string,
+  newText: string
+): number {
   // loop through each character and see if they match
-  for (let i in oldText) {
-    // if they don't match, return the index where the difference occurs
-    if (oldText[i] !== newTextArr[i]) {
-      return parseInt(i)
-    }
-  }
+  // if they don't match, return the index where the difference occurs
+  const nonMatchingIndex = oldText
+    .split('')
+    .findIndex((letter, i) => letter !== newText[i])
 
   // if we get through all of oldText without finding a match then the difference is at the end
-  return oldText.length
+  return nonMatchingIndex >= 0 ? nonMatchingIndex : oldText.length
 }
 </script>
 
@@ -230,6 +277,7 @@ form {
   border-radius: var(--rounded-md);
   padding: var(--size-1) 0;
   list-style-type: none;
+  z-index: 50;
 }
 
 #search-results-list li {
