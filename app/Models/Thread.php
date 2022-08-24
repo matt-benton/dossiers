97f54887caa208f6107d4d5b04ea4c6770e3dce2
matt-benton <mattbenton@hey.com>
2022-08-24 09:23:47 -0500

    Clean up threadables table when deleting thread

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Thread extends Model
 {
+    protected static function booted()
+    {
+      // clean up the threadables table after deleting thread
+      static::deleted(function ($thread) {
+        DB::table('threadables')->where('thread_id', $thread->id)->delete();
+      });
+    }
+
     public function people()
     {
       return $this->morphedByMany(Person::class, 'threadable');
