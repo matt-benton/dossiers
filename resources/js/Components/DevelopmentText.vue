b630c915a847747328d56f389b03580e8891e7da
matt-benton <mattbenton@hey.com>
2022-08-29 10:40:58 -0500

    Use regex to highlight people/interest in string

--- resources/js/Components/DevelopmentText.vue
+++ resources/js/Components/DevelopmentText.vue
@@ -20,59 +20,19 @@ const developmentText = computed(() => {
   // loop through all the people tagged in this development
   // and insert tags to highlight their names
   props.peopleInThread.forEach((person) => {
-    insertedText = highlightStringInText(insertedText, person.name)
+    insertedText = insertedText.replaceAll(
+      `@${person.name}`,
+      `<b>@${person.name}</b>`
+    )
   })
 
   props.interestsInThread.forEach((interest) => {
-    insertedText = highlightStringInText(insertedText, interest.name)
+    insertedText = insertedText.replaceAll(
+      `@${interest.name}`,
+      `<b>@${interest.name}</b>`
+    )
   })
 
   return insertedText
 })
-
-const findNameInText = function (
-  text: string,
-  personName: string
-): { start: number | null; end: number | null } {
-  const startIndex = text
-    .split('')
-    .findIndex(
-      (letter, index) =>
-        text.slice(index, index + personName.length) === personName
-    )
-
-  return startIndex >= 0
-    ? { start: startIndex, end: startIndex + personName.length }
-    : { start: null, end: null }
-}
-
-const insertIntoMidString = function (
-  string: string,
-  insertText: string,
-  index: number
-) {
-  return (
-    string.slice(0, index) + insertText + string.slice(index, string.length)
-  )
-}
-
-function highlightStringInText(text: string, str: string): string {
-  // get the index of the start and end of this person's name in the text
-  const nameLoc = findNameInText(text, `@${str}`)
-
-  if (nameLoc.start === null || nameLoc.end === null) {
-    return text
-  }
-
-  // this is a variable so we can get the length for the second insert
-  const firstInsertText = '<b>'
-
-  // insert the beginning tags
-  text = insertIntoMidString(text, firstInsertText, nameLoc.start)
-
-  // insert the end tags
-  text = insertIntoMidString(text, '</b>', nameLoc.end + firstInsertText.length)
-
-  return text
-}
 </script>
